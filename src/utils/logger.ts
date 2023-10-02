134bf88ebc192219db4853cb3a948bad27d7c441
David Sellam <37403593+dvd90@users.noreply.github.com>
2023-10-02 17:43:41 +0300

    fix deploy

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,4 +1,6 @@
-import chalk from 'chalk';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const chalk = require('chalk');
+
 import * as Sentry from '@sentry/node';
 import { LeanRequest } from './interfaces';
 
